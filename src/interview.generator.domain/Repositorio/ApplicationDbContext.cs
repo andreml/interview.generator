6fa4556f0024bc4ff67a2aeb6c3920218c4b12b2
Guilherme.Taira <gui.s.taira@live.com>
2023-10-18 22:03:54 -0300

    Adicionando DbContext

---
+++ src/interview.generator.domain/Repositorio/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using interview.generator.domain.Entidade;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace interview.generator.domain.Repositorio
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Alternativa> Alternativa { get; set; }
+        public DbSet<AreaConhecimento> AreaConhecimento { get; set; }
+        public DbSet<Avaliacao> Avaliacao { get; set; }
+        public DbSet<Perfil> Perfil { get; set; }
+        public DbSet<Pergunta> Pergunta { get; set; }
+        public DbSet<PerguntaQuestionario> PerguntaQuestionario { get; set; }
+        public DbSet<Questionario> Questionario { get; set; }
+        public DbSet<RespostaAvaliacao> RespostaAvaliacao { get; set; }
+        public DbSet<TipoQuestionario> TipoQuestionario { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetValue<string>("ConnectionStrings:DataBase"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
