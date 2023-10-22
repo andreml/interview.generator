f2619500a179ce99a8e82e5fd8d91d494f268cc6
Guilherme.Taira <gui.s.taira@live.com>
2023-10-22 17:49:30 -0300

    Criando usuario view model para retorno de dados

--- src/interview.generator.application/Interfaces/IUsuarioService.cs
+++ src/interview.generator.application/Interfaces/IUsuarioService.cs
@@ -1,13 +1,13 @@
 ﻿using interview.generator.application.Dto;
-using interview.generator.domain.Entidade;
+using interview.generator.application.ViewModels;
 using interview.generator.domain.Entidade.Common;
 
 namespace interview.generator.application.Interfaces
 {
     public interface IUsuarioService
     {
-        Task<ResponseBase<Usuario>> ObterUsuario(Guid id);
-        Task<ResponseBase<IEnumerable<Usuario>>> ListarUsuarios();
+        Task<ResponseBase<UsuarioViewModel>> ObterUsuario(Guid id);
+        Task<ResponseBase<IEnumerable<UsuarioViewModel>>> ListarUsuarios();
         Task<ResponseBase> CadastrarUsuario(AdicionarUsuarioDto usuario);
         Task<ResponseBase> AlterarUsuario(AlterarUsuarioDto usuario);
         Task<ResponseBase> ExcluirUsuario(Guid id);
