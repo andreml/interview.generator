312a21bc6360ee1ebb653bb5ae741808e41706c7
Guilherme.Taira <gui.s.taira@live.com>
2023-10-22 09:08:08 -0300

    Adicionando Login e Senha no AlterarUsuarioDto e melhorando validações

--- src/interview.generator.application/Dto/AlterarUsuarioDto.cs
+++ src/interview.generator.application/Dto/AlterarUsuarioDto.cs
@@ -10,6 +10,8 @@ namespace interview.generator.application.Dto
         public string Cpf { get; set; } = default!;
         public string Nome { get; set; } = default!;
         public Perfil Perfil { get; set; } = default!;
+        public string Login { get; set; } = default!;
+        public string Senha { get; set; } = default!;
     }
 
     public class AlterarUsuarioDtoValidator : AbstractValidator<AlterarUsuarioDto>
@@ -17,9 +19,29 @@ namespace interview.generator.application.Dto
         public AlterarUsuarioDtoValidator()
         {
             RuleFor(x => x.Id).NotNull().WithMessage("Id é obrigatório");
+
             RuleFor(x => x.Cpf).NotNull().WithMessage("Cpf é obrigatório");
-            RuleFor(x => x.Cpf).Must(document => ValidateDocument.IsCpf(document)).WithMessage("Documento inválido");
-            RuleFor(x => x.Nome).NotNull().WithMessage("Nome é obrigatório");
+
+            RuleFor(x => x.Cpf).Must(ValidateDocument.IsCpf).WithMessage("Documento inválido");
+
+            RuleFor(x => x.Nome)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Nome é obrigatório")
+                .MaximumLength(30)
+                .WithMessage("Login deve ter até 100 caracteres");
+
+            RuleFor(x => x.Login)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Login é obrigatório")
+                .MaximumLength(30)
+                .WithMessage("Login deve ter até 30 caracteres");
+
+            RuleFor(x => x.Senha)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Senha é obrigatória");
         }
     }
 }
