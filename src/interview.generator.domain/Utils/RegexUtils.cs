0e2a795eaa0ca24e855dab70ba9832f2ed493a1e
Guilherme.Taira <gui.s.taira@live.com>
2023-10-25 08:55:03 -0300

    Adicionando validações de senha e login

---
+++ src/interview.generator.domain/Utils/RegexUtils.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace interview.generator.domain.Utils
+{
+    public static class RegexUtils
+    {
+        public const string LoginValidador = @"^[\w.]+$";
+        public const string SenhaValidator = @"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+    }
+}
