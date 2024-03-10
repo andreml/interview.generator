20d72f98e1e9fd95cf901999e769a304f8322fef
Guilherme.Taira <gui.s.taira@live.com>
2024-03-10 16:12:18 -0300

    Refatoração

--- src/InterviewGenerator.Application/Services/MassTransitService.cs
+++ src/InterviewGenerator.Application/Services/MassTransitService.cs
@@ -1,13 +1,6 @@
 ﻿using InterviewGenerator.Application.Interfaces;
-using InterviewGenerator.Domain.Entidade;
 using InterviewGenerator.Domain.Entidade.Common;
 using MassTransit;
-using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InterviewGenerator.Application.Services
 {
@@ -18,15 +11,10 @@ namespace InterviewGenerator.Application.Services
         {
             _bus = bus;
         }
-        public Task<ResponseBase> InserirEmFilaDeErro()
-        {
-            throw new NotImplementedException();
-        }
 
         public async Task<ResponseBase> InserirMensagem(object mensagem, string fila)
         {
             var response = new ResponseBase();
-
 
             var endpoint = await _bus.GetSendEndpoint(new Uri($"queue:{fila}"));
 
